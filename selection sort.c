99a2f697f549564b8f232746bd8c9e2dc360dc69
qlj <2461670760@qq.com>
2025-09-18 23:07:52 +0800

    update new sort

---
+++ selection sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int arr1[] = {4,5,7,2,54,65,456,345,234,2};
+    int len = sizeof(arr1) / sizeof(arr1[0]);
+    int term = 0;
+    for(int i = 0; i < len-1; i++){
+        int min = i;
+        for(int j = i+1; j < len; j++){
+            if(arr1[j] < arr1[min]){
+                min = j;
+            }
+        }
+        if(min != i){
+            term = arr1[i];
+            arr1[i] = arr1[min];
+            arr1[min] = term;
+        }
+    }
+    printf("排序后的数组为：");
+    for(int i = 0; i < len; i++){
+        printf("%d ", arr1[i]);
+    }
+    return 0;
+}
