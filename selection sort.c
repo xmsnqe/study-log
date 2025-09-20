66022a8abdb1cb7644c9f49a674e67ab869ca58b
xmsnqe <2461670760@qq.com>
2025-09-20 21:26:36 +0800

    update

--- selection sort.c
+++ selection sort.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
+    /*
+    选择排序
+    时间复杂度：O(n^2) 2 * t(for())
+    空间复杂度：O(1)
+    */
+    
     int arr1[] = {4,5,7,2,54,65,456,345,234,2};
     int len = sizeof(arr1) / sizeof(arr1[0]);
     int term = 0;
