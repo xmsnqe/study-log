1ea6596c11577678c2f22bada1cc468847066af5
xmsnqe <2461670760@qq.com>
2025-09-21 10:23:45 +0800

    update

--- selection sort.c
+++ selection sort.c
@@ -5,8 +5,7 @@ int main(){
     时间复杂度：O(n^2) 2 * t(for())
     空间复杂度：O(1)
     */
-    
-    int arr1[] = {4,5,7,2,54,65,456,345,234,2};
+    int arr1[] = {4,5,7,2,54,65,456,345,234,2,35};
     int len = sizeof(arr1) / sizeof(arr1[0]);
     int term = 0;
     for(int i = 0; i < len-1; i++){
