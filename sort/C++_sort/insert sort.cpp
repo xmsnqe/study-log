ff58d1765319b1e39154b0a7d6eb3cf421e5dcd0
xmsnqe <2461670760@qq.com>
2025-10-21 21:49:17 +0800

    update

--- sort/C++_sort/insert sort.cpp
+++ sort/C++_sort/insert sort.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/*
+插入排序
+时间复杂度：O(n^2)
+空间复杂度：O(1)
+*/ */
 void insertSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
