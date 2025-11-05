2f1a962c60cdf8d654e6044fd56b9d473c4c2600
xmsnqe <2461670760@qq.com>
2025-11-05 23:41:28 +0800

    NULL->nullptr

---
+++ structure/C++/stack_linked list.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+class LinkedListStack {
+    private:
+        ListNode *stackTop;
+        int stkSize;
+
+         //释放链表内存函数（用于析构函数）
+        void freeMemoryLinkedList(ListNode *node) {
+            while (node != nullptr) {
+                ListNode *tmp = node;
+                node = node->next;
+                delete tmp;
+            }
+        }
+    public:
+        LinkedListStack() {
+            stackTop = nullptr;
+            stkSize = 0;
+        }
+        ~LinkedListStack() {
+            freeMemoryLinkedList(stackTop);
+        }
+        int size() {
+            return stkSize;
+        }
+
+        bool isEmpty() {
+            return size() == 0;
+        }
+        /*入栈 */
+        void push(int num) {
+            ListNode *node = new ListNode(num);
+            node->next = stackTop;
+            stackTop = node;
+            stkSize++;
+        }
+        /*出栈 */
+        int pop() {
+            int num = top();
+            ListNode *tmp = stackTop;
+            stackTop = stackTop->next;
+            delete tmp;
+            stkSize--;
+            return num;
+        }
+        /*访问栈顶元素 */
+        int top() {
+            if(isEmpty()){
+                throw out_of_range("栈为空");
+            }
+            return stackTop->val;
+        }
+        /*将链表转化为 Vector 用于打印 */
+        vector<int> toVector() {
+            vector<int> vec;
+            ListNode *node = stackTop;
+            while (node != nullptr) {
+                vec.push_back(node->val);
+                node = node->next;
+            }
+            reverse(vec.begin(), vec.end());
+            return vec;
+        }
+};
